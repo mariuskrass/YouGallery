f56f27dabd274ade3f758b53ae34b9f22477bf7a
Remo Rothacher <rothacher.remo1@gmail.com>
2017-09-20 15:27:11 +0200

    Absolut endgültige Abgabe :+1:

--- repository/PictureRepository.php
+++ repository/PictureRepository.php
@@ -35,7 +35,7 @@ class PictureRepository extends Repository
         $query = "SELECT u.id as userId, u.username, u.profile_picture, p.id, p.name, p.upload_date FROM user_follows_user ufu
             LEFT JOIN picture AS p ON p.user_id = ufu.user2_id
             LEFT JOIN user AS u ON u.id = ufu.user2_id
-            WHERE ufu.user1_id = ?
+            WHERE ufu.user1_id = ? AND p.id IS NOT NULL
             ORDER BY p.upload_date DESC;";
 
         $statement = ConnectionHandler::getConnection()->prepare($query);
