4ed99d4603dbea5fd42ba7f411c0589798e9c16c
Remo Rothacher <rothacher.remo1@gmail.com>
2017-09-20 14:55:51 +0200

    add profile picture on hot

--- view/hot.php
+++ view/hot.php
@@ -13,12 +13,18 @@ foreach($pictures as $picture) :?>
 
 <?php
 $picture->isLiked ? $heartClass = "glyphicon glyphicon-heart heartred" : $heartClass = "glyphicon glyphicon-heart-empty";
+
+if($picture->profile_picture != null){
+    $imagesrc = "var/www/uploads/" . $picture->profile_picture;
+}else{
+    $imagesrc = "images/profile.png";
+}
 ?>
 
 <div id="feed">
     <div class="feed-element">
         <div class="feed-header feed-username">
-            <img class="feed-profilbild" src='/images/profile.png'>
+            <img class="feed-profilbild" src='<?=$imagesrc?>'>
             <a class="feed-usernam" href="/profile?userId=<?=$picture->userId?>"><?php echo $picture->username ?></a>
         </div>
         <div id="functionside">
